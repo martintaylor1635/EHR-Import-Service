8067696861166af74173b0f6d19c4de2a32e451e
martintaylor1635 <martintaylor1635@gmail.com>
2024-05-04 15:31:27 +0100

    Initial Commit

---
+++ src/main/java/dev/martintaylor/ehrimportservice/EhrImportServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.martintaylor.ehrimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrImportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhrImportServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/martintaylor/ehrimportservice/EhrImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.martintaylor.ehrimportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrImportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
