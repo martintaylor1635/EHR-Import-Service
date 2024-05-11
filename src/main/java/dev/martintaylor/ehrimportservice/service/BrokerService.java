afe3842df0f02c638903be073d086e2d03de9183
martintaylor1635 <martintaylor1635@gmail.com>
2024-05-12 00:10:15 +0100

    Added initial BrokerService.java

---
+++ src/main/java/dev/martintaylor/ehrimportservice/service/BrokerService.java
@@ -0,0 +1,10 @@
+package dev.martintaylor.ehrimportservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BrokerService {
+    public void handleMessage() {
+        throw new UnsupportedOperationException();
+    }
+}
