f90465b460c9ca4889a8fec54815843d33a4f128
martintaylor1635 <martintaylor1635@gmail.com>
2024-05-15 17:58:34 +0100

    Enhanced UploadException.java base exception.

--- src/main/java/dev/martintaylor/ehrimportservice/exception/base/UploadException.java
+++ src/main/java/dev/martintaylor/ehrimportservice/exception/base/UploadException.java
@@ -1,7 +1,25 @@
 package dev.martintaylor.ehrimportservice.exception.base;
 
+import java.util.UUID;
+
+import static dev.martintaylor.ehrimportservice.utility.DateUtility.getZonedIsoOffsetDateTime;
+
+
 public abstract class UploadException extends RuntimeException {
-    public UploadException(String message) {
+    private final UUID conversationId;
+    private final String thrownAt;
+
+    public UUID getConversationId() {
+        return conversationId;
+    }
+
+    public String getThrownAt() {
+        return thrownAt;
+    }
+
+    public UploadException(UUID conversationId, String message) {
         super(message);
+        this.conversationId = conversationId;
+        this.thrownAt = getZonedIsoOffsetDateTime();
     }
 }
